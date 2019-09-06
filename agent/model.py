b49c2e4850aef6b380e699a24e4e1ee278720fdf
dpong <powenperng@gmail.com>
2019-09-06 14:31:54 +0800

    Model modified.

--- agent/model.py
+++ agent/model.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Add, Subtract, Lambda, BatchNormalization, concatenate
-from tensorflow.keras.layers import Conv1D, Flatten, MaxPooling1D, GlobalAveragePooling1D, Reshape
+from tensorflow.keras.layers import Conv1D, Flatten, MaxPooling1D, GlobalAveragePooling1D, Embedding
 from tensorflow.keras.models import Model
 #from agent.noisydense import NoisyDense
 import tensorflow.keras.backend as K
@@ -14,7 +14,7 @@ class Build_model():
         norm = BatchNormalization()(state_input)  # 輸入標準化
         # 額外輸入
         self_state_input = Input(shape=(self_feat_shape[-2], self_feat_shape[-1]), name='self_state_input', dtype='float64')
-        s1 = Conv1D(self_feat_shape[-1], 1, padding='same', activation='relu')(self_state_input)
+        s1 = Embedding(self_feat_shape[-1], self_feat_shape[-1], input_length=self_feat_shape[-2])(self_state_input)
         flat_s1 = Flatten()(s1)
         flat_norm_s1 = BatchNormalization()(flat_s1)
         # 卷積層們，kernel_size為5天，一週的概念
